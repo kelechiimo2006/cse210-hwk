4a7a8b45962c5cedf81619743a3cc5368f8c5f0a
Samuel-David Imo <kelechiimo2006@gmail.com>
2025-01-12 15:14:31 -0700

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter;
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        grade = int.Parse(userInput);
+
+        if (grade > 90 || grade == 90)
+        {
+            letter = "A";
+        }
+        else if (grade > 80 || grade == 80)
+        {
+            letter = "B";
+        }
+        else if (grade > 70 || grade == 70)
+        {
+            letter = "C";
+        }
+        else if (grade > 60 || grade == 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
     }
 }
