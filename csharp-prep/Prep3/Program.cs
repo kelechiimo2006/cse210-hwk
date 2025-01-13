eb33a8453dd86bcf26c46788f46909f28056eda2
Samuel-David Imo <kelechiimo2006@gmail.com>
2025-01-12 19:20:14 -0700

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,5);
+        string userInput;
+        int guess;
+        
+        do
+        {
+            
+            Console.Write("What is your guess? ");
+            userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        }while (guess != number);
+        Console.WriteLine($"You guessed it! {number}");
     }
 }
